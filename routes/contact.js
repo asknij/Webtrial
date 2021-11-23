e0d3d788103bf45b671aef08d8d126d3488cd825
ANANTHU SANTHOSH <asknij@gmail.com>
2021-11-23 01:36:11 -0500

    all text coverted

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/',(req,res,next) => {
-  res.render('contact');
+  res.render('contact',{
+    info:'Usually available from Monday to friday 8 AM to 7 PM. Feel free to contact during that time after that please leave me an email and i will reply as soon as possible.'
+  });
 });
 
 module.exports = router;
--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-    res.render('Projects/index');
+    res.render('Projects/index',{
+        Android:'Android Applications Development   :   I am particularly intrested in Android applications development and i had worked a few years in that platform.',
+        Deep:'Machine Learning   :   I have a few months experience assisting one of my friend in a deep learning project and it is an area in which i like to have some knowledge in.',
+        Web:'Web Development   :   In this mordern world wbe development shows a promising future and hence i am intrested in web applications development.',
+        Digital:'Digital Marketing   :   One of the main marketing strategies of the mordern era and with a bright opportunity for development in the near future.'
+    });
 });
 
 module.exports = router
