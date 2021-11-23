e1a1714677bcd7be24e693c353201796a9ea5b54
ANANTHU SANTHOSH <asknij@gmail.com>
2021-11-23 00:07:32 -0500

    contact page done

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About this Page', 
-  pageText:'Here is some info about this Project',
+  pageText1:'About Me',
   var:'This about my page'
  });
 });
 
 router.get ('/', function(req,res,next) {
-  res.render('index',{title:'Student/Freelance Developer'});
+  res.render('index',{pageText:'About Me'});
 });
 
 
